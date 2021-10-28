7366ed30ac0d746a8d2a6291ea7c6a882cbe9d2e
moogieon <vzlzydn5007@gmail.com>
2021-10-28 22:31:53 +0900

    ++

--- src/components/units/list/test.tsx
+++ src/components/units/list/test.tsx
@@ -1,4 +1,8 @@
 import { Wrap } from "./marketList.style";
 export default function Test() {
-  return <Wrap>페이지네이션</Wrap>;
+  return;
+  <>
+    <Wrap>페이지네이션</Wrap>
+    <div>{Array}</div>
+  </>;
 }
